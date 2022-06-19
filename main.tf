e1c6e8ba1a6ef4bac870b67f208e632dc94d5ac3
m4rlonalvesilva <marlonesx@gmail.com>
2022-06-19 01:36:14 -0300

    first commit

--- main.tf
+++ main.tf
@@ -3,45 +3,20 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.94.0"
-    }
-
     aws = {
       source  = "hashicorp/aws"
-      version = "3.73.0"
+      version = "4.18.0"
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "remote-state"
-    storage_account_name = "danielgilremotestate"
-    container_name       = "remote-state"
-    key                  = "pipeline-github-actions/terraform.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-data "terraform_remote_state" "vnet" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "remote-state"
-    storage_account_name = "danielgilremotestate"
-    container_name       = "remote-state"
-    key                  = "azure-vnet/terraform.tfstate"
-  }
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-east-1"
 
   default_tags {
     tags = {
-      owner      = "danielgil"
+      owner      = "marlon"
       managed-by = "terraform"
     }
   }
@@ -50,8 +25,8 @@ provider "aws" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "danielgil-remote-state"
+    bucket = "marlon-remote-state"
     key    = "aws-vpc/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
--- vm-aws.tf
+++ vm-aws.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "key" {
 }
 
 resource "aws_instance" "vm" {
-  ami                         = "ami-0d527b8c289b4af7f"
+  ami                         = "ami-052efd3df9dad4825"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
